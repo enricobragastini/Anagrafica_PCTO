61f5c7f70c7161c10b65bea7af2f60718d5107be
Stini01 <enri.braga@gmail.com>
2020-01-19 11:50:14 +0100

    Minor login changes

--- bin/index.php
+++ bin/index.php
@@ -24,58 +24,66 @@
   <link rel="shortcut icon" type="image/png" href="img/business.png"/>
 </head>
 
-<body>
+<body class="grey lighten-3">
 
-  <body class="grey lighten-3">
-
-    <div class="navbar-fixed">
-      <nav>
-        <div class="box">
-          <div class="nav-wrapper green">
-            <a href="#" class="brand-logo left"><i class="material-icons">work</i>Speedy PCTO</a>
-            <a href="#" class="brand-logo right show-on-medium-and-up hide-on-small-only">ITI G. Marconi</a>
-          </div>
+  <!-- NavBar -->
+  <div class="navbar-fixed">
+    <nav>
+      <div class="box">
+        <div class="nav-wrapper green">
+          <a href="#" class="brand-logo left"><i class="material-icons">work</i>Speedy PCTO</a>
+          <a href="#" class="brand-logo right show-on-medium-and-up hide-on-small-only">ITI G. Marconi</a>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
+  </div>
 
-    <div class="box white z-depth-3 center" id="content">
-      <h4 class="animated bounceIn">Sign In</h4>
-      <p class="animated bounceIn delay-500ms"><b>Attenzione!</b> Per proseguire occorre fare il login.<br><b>Hai bisogno di un account?</b> Contatta il docente che gestisce il PCTO!</p>
-      <div class="divider"></div>
+  <!-- Main content -->
+  <div class="box white z-depth-3 center" id="content">
+    <h4 class="animated bounceIn">Sign In</h4>
+    <p class="animated bounceIn delay-500ms"><b>Attenzione!</b> Per proseguire occorre fare il login.<br><b>Hai bisogno di un account?</b> Contatta il docente che gestisce il PCTO!</p>
+    <div class="divider"></div>
 
-      <!-- Login Form -->
-      <form class="" action="index.php" method="post">
-        <div class="row animated bounceInUp delay-500ms" style="padding-top: 2.5rem;">
-          <div class="col m8 offset-m2 s12">
-            <div class="row">
-              <div class="input-field col s12">
-                <i class="material-icons prefix">account_circle</i>
-                <input id="username" type="text" class="validate" name="username">
-                <label for="username">Username</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col s12">
-                <i class="material-icons prefix">lock</i>
-                <input id="password" type="password" class="validate" name="password">
-                <label for="password">Password</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col s12">
-                <button class="btn waves-effect waves-light" type="submit" name="action" style="background-color: #459f47;">LOGIN
-                  <i class="material-icons right">send</i>
-                </button>
-              </div>
+    <!-- Login Form -->
+    <form class="" action="index.php" method="post">
+      <div class="row animated bounceInUp delay-500ms" style="padding-top: 2.5rem;">
+        <div class="col m8 offset-m2 s12">
+          <div class="row">
+            <h6>Inserisci le tue credenziali</h6>
+            <div class="input-field col s12">
+              <i class="material-icons prefix">account_circle</i>
+              <input id="username" type="text" class="validate" name="username">
+              <label for="username">Username</label>
             </div>
           </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix">lock</i>
+              <input id="password" type="password" class="validate" name="password">
+              <label for="password">Password</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col s12">
+              <!-- Testo da mostrare in caso di credenziali errate -> DA FARE IN PHP -->
+              <!-- <p class="red-text">Credenziali errate, riprova!</p> -->
+              <button class="btn waves-effect waves-light" type="submit" name="action" style="background-color: #459f47;">ACCEDI
+                <i class="material-icons right">send</i>
+              </button>
+            </div>
+          </div>
         </div>
+      </div>
+    </form>
+  </div>
 
-      </form>
+  <div class="row center">
+    <div class="col s12">
+      <p>&copy; <?php echo date("Y"); ?> - Enrico Bragastini, Loris Pesarin</p>
     </div>
+  </div>
 
-  </body>
+
 
 </body>
 </html>
